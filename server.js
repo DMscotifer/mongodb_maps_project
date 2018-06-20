30de9f7e7c4dcbd14a1cd1dd9c81d03be6a20e8e
Scott Stevenson <scotifer92@googlemail.com>
2018-06-20 15:32:02 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const parser = require('body-parser');
+const server = express();
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+config = {
+  entry: __dirname + "/client/src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/client/build"
+  },
+  devtool: "source-map"
+}
+
+module.exports = config;
+
